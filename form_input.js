830ff09d2ddb0b3db0a6479899a3d732e8e2929c
panda495 <srens1226@gmail.com>
2024-07-26 01:36:45 +0800

    Update form_input.js

--- form_input.js
+++ form_input.js
@@ -25,10 +25,10 @@ var values = {
 
 const text_list = {
   'addres': 3730012,
-  'zip': 3730012',
+  'zip': 3730012,
   'mail': 'test@test.com',
-  'phone': '090123145678',
-  'tel': '090123145678',
+  'phone': 090123145678,
+  'tel': 090123145678,
   'name': 'テスト'
 };
 
