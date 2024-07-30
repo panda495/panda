65c0d92a5fe316753bd9f3dc546270a7909927a7
panda495 <srens1226@gmail.com>
2024-07-30 11:32:45 +0800

    Update getInfo.js

--- getInfo.js
+++ getInfo.js
@@ -2,6 +2,7 @@
 // javascript:(function(url){var s=document.createElement('script');s.src=url+'?t=%27+new Date().getTime();document.body.appendChild(s);})(%27https://panda495.github.io/panda/getInfo.js%27);
 javascript:(function() {
     // CMSリストの宣言
+     const cms_list0729 = {}
     // const cms_list0729 = {
     //     "Makeshop": {
     //         email: '[MAIL_ADDRESS]',
